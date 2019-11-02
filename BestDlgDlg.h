48f6db376794991486f31c5d5095c7fb0bb5fa48
namtaegyu <54826784+namtaegyu@users.noreply.github.com>
2019-11-02 20:32:53 +0900

    Add files via upload

---
+++ BestDlgDlg.h
@@ -0,0 +1,41 @@
+﻿
+// BestDlgDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CBestDlgDlg 대화 상자
+class CBestDlgDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CBestDlgDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_BESTDLG_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	
+	afx_msg void OnBnClickedButton1();
+
+	
+	CListBox m_List;
+	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
+};
